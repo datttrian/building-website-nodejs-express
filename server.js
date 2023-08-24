8b65c7d892f2ee80085c465fb4474ff5b34f3474
datttrian <datttrian@gmail.com>
2023-08-24 19:52:09 +0000

    Add a session management middleware

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session');
+const rand = require('random-key');
 
 const FeedbackService = require('./services/FeedbackService');
 const SpeakersService = require('./services/SpeakerService');
@@ -12,6 +14,15 @@ const routes = require('./routes');
 const app = express();
 
 const port = 3000;
+
+app.set('trust proxy', 1);
+
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: [rand.generate(), rand.generate()],
+  })
+);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
