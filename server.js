2d5206be4bbca06a22d73b8ed92fc7457b2f7311
datttrian <datttrian@gmail.com>
2023-08-24 17:05:03 +0000

    Modular routes with express.Router

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 const express = require('express');
 const path = require('path');
 
+const routes = require('./routes');
+
 const app = express();
 
 const port = 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
-console.log('restarting');
 
 app.use(express.static(path.join(__dirname, './static')));
 
-app.get('/', (request, response) => {
-  response.render('pages/index', { pageTitle: 'Welcome' });
-});
-
-app.get('/speakers', (request, response) => {
-  response.sendFile(path.join(__dirname, './static/speakers.html'));
-});
+app.use('/', routes());
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}!`);
